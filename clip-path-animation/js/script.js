708057acfbc24360998cbb4236dfd15cd1cb336b
Atul Sharma <37208891+attulsharmma@users.noreply.github.com>
2019-10-21 16:22:54 +0530

    Add files via upload

---
+++ clip-path-animation/js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+ $('span.iclick').click(function(){
+    //  $('.inner').toggleClass({"background-color": "#fd8cb2", "clip-path": "circle(75%)"});
+     $('.inner').toggleClass('clipping-path-full');
+    
+ });
+
+});
